7071481ef508689ef6c3f12f446d8350e99fb01a
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2025-02-24 15:08:50 +0545

    Refactor chat message rendering and update form submission to use HTMX for dynamic message loading

--- a_rtchat/views.py
+++ a_rtchat/views.py
@@ -10,14 +10,15 @@ def chat_view(request):
     chat_messages = chat_group.chat_messages.all()[:33]
 
     form = ChatmessageCreateForm()
-    if request.method == "POST":
+    if request.htmx:
         form = ChatmessageCreateForm(request.POST)
         if form.is_valid():
             message = form.save(commit=False)
             message.author = request.user
             message.group = chat_group
             message.save()
-            redirect("home")
+            context = {"message": message, "user": request.user}
+            return render(request, "a_rtchat/partials/chat_message_p.html", context)
 
     return render(
         request, "a_rtchat/chat.html", {"chat_messages": chat_messages, "form": form}
